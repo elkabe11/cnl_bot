5de73bb00700ca4ba9638efdd930d7a41e6ec17a
elkabe11 <38818024+elkabe11@users.noreply.github.com>
2018-04-28 17:15:33 -0300

    Update program.py

--- src/program.py
+++ src/program.py
@@ -71,15 +71,20 @@ while True:
             if event.code == ecodes.REL_Y:
                 if event.value <= -2:
                     atras()
+                    
                 elif event.value >= 2:
                     adelante()
+                    
                 else:
                     parar()
+                    
             if event.code == ecodes.REL_X:
                 if event.value <= -2:
                     derecha()
+                    
                 elif event.value >= 2:
                     izquierda()
+                    
                 else:
                     parar()
 
