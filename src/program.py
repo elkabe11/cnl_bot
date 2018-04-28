11a0cff10503054cd421df5ff36a0a76a00a073d
elkabe11 <38818024+elkabe11@users.noreply.github.com>
2018-04-28 17:04:42 -0300

    Update program.py

--- src/program.py
+++ src/program.py
@@ -12,30 +12,35 @@ RUTA='/dev/input/event0'
 def adelante():
     wiringpi.digitalWrite(21, 1)
     wiringpi.digitalWrite(20, 0)
+    
     wiringpi.digitalWrite(16, 1)
     wiringpi.digitalWrite(12, 0)
 
 def atras():
     wiringpi.digitalWrite(21, 0)
     wiringpi.digitalWrite(20, 1)
+    
     wiringpi.digitalWrite(16, 0)
     wiringpi.digitalWrite(12, 1)
 
 def parar():
     wiringpi.digitalWrite(21, 0)
     wiringpi.digitalWrite(20, 0)
+    
     wiringpi.digitalWrite(16, 0)
     wiringpi.digitalWrite(12, 0)
 
 def izquierda():
     wiringpi.digitalWrite(21, 0)
     wiringpi.digitalWrite(20, 1)
+    
     wiringpi.digitalWrite(16, 1)
     wiringpi.digitalWrite(12, 0)
 
 def derecha():
     wiringpi.digitalWrite(21, 1)
     wiringpi.digitalWrite(20, 0)
+    
     wiringpi.digitalWrite(16, 0)
     wiringpi.digitalWrite(12, 1)
 
@@ -65,17 +70,17 @@ while True:
             print("rel: " + str(event.value))
             if event.code == ecodes.REL_Y:
                 if event.value <= -2:
-                    adelante()
-                elif event.value >= 2:
                     atras()
+                elif event.value >= 2:
+                    adelante()
                 else:
-                    parar()
+                    adelante()
             if event.code == ecodes.REL_X:
                 if event.value <= -2:
-                    izquierda()
-                elif event.value >= 2:
                     derecha()
+                elif event.value >= 2:
+                    izquierda()
                 else:
-                    parar()
+                    adelante()
 
     n.notify("WATCHDOG=1")
